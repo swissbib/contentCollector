faba3e5f26e618fe585c16f32622432818b4c65e
Guenter Hipler <guenter.hipler@unibas.ch>
2015-12-15 14:33:59 +0100

    rm old nebis shell script an additional shellscript to start nebis and rero file based tasks within a test environment

--- bin/process-nebis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-PROCESS_DIR=/swissbib/harvesting/bin
-RUNDIR=/swissbib/harvesting/rundir
-
-cd ${PROCESS_DIR}
-
-python swissbibNebisClient.py --config=${HOME}/confdir/config.nebis.prod.xml >> ${RUNDIR}/process-nebis-python.log 2>&1
