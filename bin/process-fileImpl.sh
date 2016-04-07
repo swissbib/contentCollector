3386f28ccdd2c06d86f48509dc208e9e49c55ad5
Guenter Hipler <guenter.hipler@unibas.ch>
2016-04-07 19:58:09 +0200

    include lock mechanism for file processing between sequential processes

--- bin/process-fileImpl.sh
+++ bin/process-fileImpl.sh
@@ -8,10 +8,29 @@ RUNDIR=${DATA_BASE_DIR}/rundir
 CONFDIR=${CONTENTENV_HOME}/confdir
 #PROCESS_DIR=/home/harvester/envContentCollector/bin
 
-
+#GH, 7.4.2016
+#we need a blocking mechanism if another process with the same repository is already running
 
 CONFFILE=$1
 
-cd ${PROCESS_DIR}
+fileNameOnly=$(basename "$CONFFILE")
 
-${VIRTUAL_PYTHON_ENV}/bin/python  ${PROCESS_DIR}/FileProcessorImpl.py --config=${CONFDIR}/${CONFFILE}  >> ${RUNDIR}/processFileImpl.log 2>&1
+IFS='.' read -r -a arrayOfFileName <<< "$fileNameOnly"
+
+prefixLockfile="${arrayOfFileName[1]}"
+LOCKFILE=${CONFDIR}/${prefixLockfile}.lock
+
+if [ -e ${LOCKFILE} ]; then
+    echo -n "${arrayOfFileName[1]} is locked, probably by another process: "
+    cat ${LOCKFILE}
+    exit 1
+else
+    cd ${PROCESS_DIR}
+    echo $$ >${LOCKFILE}
+    ${VIRTUAL_PYTHON_ENV}/bin/python  ${PROCESS_DIR}/FileProcessorImpl.py --config=${CONFDIR}/${CONFFILE}  >> ${RUNDIR}/processFileImpl.log 2>&1
+    rm $LOCKFILE
+fi
+
+
+
+
