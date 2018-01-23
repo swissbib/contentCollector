0124772dc256d40d65b0edc635063be76c592165
Silvia Witzig <silvia.witzig@unibas.ch>
2018-01-23 13:44:53 +0100

    ergänze KB Thurgau

--- bin/process-harvesting.sh
+++ bin/process-harvesting.sh
@@ -7,7 +7,7 @@ RUNDIR=${DATA_BASE_DIR}/rundir
 CONFDIR=${CONTENTENV_HOME}/confdir
 
 
-DEFAULTREPOS="boris libib serval ecod snl sbt idsbb idslu idssg1 POSTERS zora retroseals abn bgr sgbn alex vaud_lib vaud_school hemu edoc"
+DEFAULTREPOS="boris libib serval ecod snl sbt idsbb idslu idssg1 POSTERS zora retroseals abn bgr sgbn alex vaud_lib vaud_school hemu edoc kbtg"
 
 
 if [ -n "$1" ]; then
